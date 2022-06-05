40a2b6d600a508d765f24b47eec9d0f0153a2e0a
Hyuto <wahyusetianto19@gmail.com>
2022-06-05 19:56:48 +0700

    Add testing script

---
+++ scripts/test/preprocessing.py
@@ -0,0 +1,35 @@
+from ..preprocessing import replace_slang, remove_noise, replace_word_elongation
+
+
+def test_replace_slang():
+    assert replace_slang("emg siapa yg nanya") == "memang siapa yang bertanya"
+    assert replace_slang("aminn") == "amin"
+
+
+def test_remove_noise():
+    assert remove_noise("saya \tmau makan\n") == "saya mau makan"
+    assert remove_noise("seneng 😄") == "seneng"
+    assert remove_noise('<a href="https://google.com">situs google</a>') == "situs google"
+    assert remove_noise("situs resmi unj https://unj.ac.id") == "situs resmi unj"
+    assert remove_noise("covid-19") == "covid"
+    assert remove_noise("kenapa emangnya!??....") == "kenapa emangnya"
+    assert (
+        remove_noise(
+            """@nonacik93 udah kedua kali ini hahaha
+            pertama ga mau vaksin, eh Qadarullah kena covid 2-2nya bulan2 lalu dan agak parah.
+            eh skrg kena lagi Ama dokter yg katanya udah dicopot itu 😣😣😣
+            ketika wa grup keluarga lebih dipercaya drpd peneliti 😅"""
+        )
+        == (
+            "nonacik udah kedua kali ini hahaha pertama ga mau vaksin eh qadarullah kena covid "
+            + "nya bulan lalu dan agak parah eh skrg kena lagi ama dokter yg katanya udah dicopot "
+            + "itu ketika wa grup keluarga lebih dipercaya drpd peneliti"
+        )
+    )
+
+
+def test_replace_word_elongation():
+    assert replace_word_elongation("kenapaa?") == "kenapa?"
+    assert replace_word_elongation("kenapaaa?") == "kenapa?"
+    assert replace_word_elongation("kenapaaaa?") == "kenapa?"
+    assert replace_word_elongation("kenapaaaaa?") == "kenapa?"
